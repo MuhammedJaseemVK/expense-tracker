57d5ce87d958258f86aa0ee01a1d964124d3ba73
Jaseem <muhammedjaseemvk96@gmail.com>
2023-12-10 12:28:54 +0530

    created login api

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,22 +1,49 @@
 const User = require('../models/user');
-const { hashPassword } = require("../helpers/authHelper");
+const { hashPassword, comparePassword, generateToken } = require("../helpers/authHelper");
 
 const registerUser = async (req, res) => {
-    const { name, email, password } = req.body;
-    if (name.length < 3 || password.length < 8 || email.match(/^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/)) {
-        return res.status(400).send({ success: 'fasle', message: 'Invalid request' });
-    }
-
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-        return res.status(400).send({ success: false, message: 'Email is already registered' });
-    }
-
     try {
+        const { name, email, password } = req.body;
+        if (name.length < 3 || password.length < 8 || !email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+            return res.status(400).send({ success: false, message: 'some fields are invalid ' });
+        }
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).send({ success: false, message: 'Email is already registered' });
+        }
+
         const hashedPassword = await hashPassword(password);
-        const newUser = new User({ name, email, password });
+        const newUser = new User({ name, email, password: hashedPassword });
         newUser.save()
-        res.status(201).json({ success: true, message: 'new user created', name, email, hashedPassword })
+        res.status(201).json({ success: true, message: 'new user created', user: newUser })
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send({ success: false, message: 'Internal Server Error' });
+    }
+}
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/) || password.length < 8) {
+            return res.status(400).send({ success: false, message: 'email or password is invalid' })
+        }
+
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.send(404).send({ success: false, message: 'Invalid email or password' });
+        }
+
+        const match = await comparePassword(password, user.password);
+        if (!match) {
+            return res.status(200).send({ success: false, message: 'Invalid email or password' })
+        }
+        const token = await generateToken(user._id);
+        if (token) {
+            res.status(201).send({ success: true, message: 'User logined', token })
+        }
     }
     catch (error) {
         console.log(error);
@@ -26,5 +53,6 @@ const registerUser = async (req, res) => {
 
 
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -1,16 +1,44 @@
 const bcrypt = require('bcrypt');
+const JWT = require("jsonwebtoken");
+const dotenv=require("dotenv")
+dotenv.config('../../');
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
 const saltRounds = 10;
 
 const hashPassword = async (password) => {
-    try{
+    try {
         const hashedPassword = await bcrypt.hash(password, saltRounds);
         return hashedPassword
     }
-    catch(error){
+    catch (error) {
         console.log(error)
+    }
+}
+
+const comparePassword = async (password, hashedPassword) => {
+    try {
+        const match = await bcrypt.compare(password, hashedPassword);
+        return match
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
+
+const generateToken = async (user) => {
+    try {
+        const token = await JWT.sign({ _id: user }, JWT_SECRET_KEY, { expiresIn: '1h' })
+        if(token){
+            return token
+        }
+    }
+    catch (error) {
+        console.error(error);
     }
 }
 
 module.exports = {
     hashPassword,
+    comparePassword,
+    generateToken
 }
