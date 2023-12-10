20922272d48bdfab83f8b767ddd8524013b03562
Jaseem <muhammedjaseemvk96@gmail.com>
2023-12-10 17:19:14 +0530

    created deleteTransaction api

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -20,7 +20,7 @@ const createTransactionController = async (req, res) => {
 const getAllTransactionsController = async (req, res) => {
     try {
         const transactions = await Transaction.find({ user: req.body.user })
-        res.status(200).send({ success: true, transactions });
+        return res.status(200).send({ success: true, transactions });
     }
     catch (error) {
         console.error(error);
@@ -35,11 +35,11 @@ const updateTransactionController = async (req, res) => {
         const updatedTransaction = { type, amount, category }
         let transaction = await Transaction.findById(id)
         if (!transaction) {
-            res.status(404).send({ success: false, message: 'No transaction found' });
+            return res.status(404).send({ success: false, message: 'No transaction found' });
         }
 
         if (transaction.user.toString() !== req.body.user) {
-            res.status(401).send({ success: false, message: 'Unauthorized access' });
+            return res.status(401).send({ success: false, message: 'Unauthorized access' });
         }
 
         transaction = await Transaction.findByIdAndUpdate(id, { $set: updatedTransaction }, { new: true })
@@ -52,9 +52,31 @@ const updateTransactionController = async (req, res) => {
     }
 }
 
+const deleteTransaction = async (req, res) => {
+    try {
+        const id = req.params.id;
+        let transaction = await Transaction.findById(id);
+        if (!transaction) {
+            return res.status(404).send({ success: false, message: 'Transaction is not found' });
+        }
+
+        if (transaction.user.toString() !== req.body.user) {
+            return res.status(404).send({ success: false, message: 'Unauthorized access' });
+        }
+
+        transaction = await Transaction.findByIdAndDelete(id);
+        res.status(200).send({ success: true, message: 'Transaction is deleted', transaction });
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).send({ success: false, message: 'Internal server error' });
+    }
+}
+
 
 module.exports = {
     createTransactionController,
     getAllTransactionsController,
-    updateTransactionController
+    updateTransactionController,
+    deleteTransaction
 }
--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const verifyUser = require("../middlewares/authMiddleware");
-const { createTransactionController, getAllTransactionsController, updateTransactionController } = require("../controllers/transactionController");
+const { createTransactionController, getAllTransactionsController, updateTransactionController,deleteTransaction } = require("../controllers/transactionController");
 
 router.post('/create', verifyUser, createTransactionController);
 router.get('/', verifyUser, getAllTransactionsController);
-router.put('/update/:id', verifyUser, updateTransactionController)
+router.put('/update/:id', verifyUser, updateTransactionController);
+router.delete('/delete/:id',verifyUser,deleteTransaction)
 
 module.exports = router;
