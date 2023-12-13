79c04f197ad9f08a59ca3110ab9c6562557e5015
Jaseem <muhammedjaseemvk96@gmail.com>
2023-12-13 11:16:17 +0530

    fixed a bug

--- client/src/components/AddTransaction.js
+++ client/src/components/AddTransaction.js
@@ -3,7 +3,7 @@ import TransactionContext from '../context/TransactionContext';
 
 function AddTransaction(props) {
 
-    const {addTransaction} =useContext(TransactionContext);
+    const { addTransaction } = useContext(TransactionContext);
 
     const [data, setData] = useState({
         amount: '',
@@ -13,28 +13,38 @@ function AddTransaction(props) {
 
 
     const handleChange = (e) => {
-        setData({...data,[e.target.name]:e.target.value});
+        setData({ ...data, [e.target.name]: e.target.value });
     }
 
-    const handleSubmit =(e)=>{
+    const handleType = (e) => {
+        setData({ ...data, type: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
         e.preventDefault();
         addTransaction(data);
-        props.showAlert('Transaction added successfully','success');
+        props.showAlert('Transaction added successfully', 'success');
+        setData({
+            amount: '',
+            type: '',
+            category: ''
+        })
     }
 
     return (
         <form style={{ maxWidth: '400px' }} onSubmit={handleSubmit}>
             <div className="mb-3">
                 <label htmlFor="amount" className="form-label">amount</label>
-                <input type="number" className="form-control" id="amount" name="amount" onChange={handleChange} required />
+                <input type="number" className="form-control" id="amount" name="amount" value={data.amount} onChange={handleChange} required />
             </div>
-            <div className="mb-3">
-                <label htmlFor="type" className="form-label">transaction type</label>
-                <input type="text" className="form-control" id="type" name="type" onChange={handleChange} required />
-            </div>
+            <select className="form-select mb-3" aria-label="Default select example" value={data.type} required onChange={handleType}>
+                <option value="" disabled>Select an option</option>
+                <option value='income'>Income</option>
+                <option value='expense'>Expense</option>
+            </select>
             <div className="mb-3">
                 <label htmlFor="category" className="form-label">Category</label>
-                <input type="text" className="form-control" id="category" name="category" onChange={handleChange} required />
+                <input type="text" className="form-control" id="category" name="category" value={data.category} onChange={handleChange} required />
             </div>
             <button type="submit" className="btn btn-primary">Add</button>
         </form>
--- client/src/components/Ledger.js
+++ client/src/components/Ledger.js
@@ -24,12 +24,17 @@ function Ledger({ showAlert }) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        updateTransaction(transaction._id,transaction);
+        updateTransaction(transaction._id, transaction);
+        showAlert('Transaction edited successfully');
         closeRef.current.click();
     }
 
     const handleChange = (e) => {
         setTransaction({ ...transaction, [e.target.name]: e.target.value });
+    }
+
+    const handleType =(e)=>{
+        setTransaction({...transaction,type:e.target.value});
     }
 
     useEffect(() => {
@@ -62,10 +67,11 @@ function Ledger({ showAlert }) {
                                     <label htmlFor="amount" className="form-label">amount</label>
                                     <input type="number" className="form-control" id="amount" name="amount" value={transaction.amount} onChange={handleChange} required />
                                 </div>
-                                <div className="mb-3">
-                                    <label htmlFor="type" className="form-label">transaction type</label>
-                                    <input type="text" className="form-control" id="type" name="type" value={transaction.type} onChange={handleChange} required />
-                                </div>
+                                <select className="form-select mb-3" aria-label="Default select example" value={transaction.type} required onChange={handleType}>
+                                    <option value="" disabled>Select an option</option>
+                                    <option value='income'>Income</option>
+                                    <option value='expense'>Expense</option>
+                                </select>
                                 <div className="mb-3">
                                     <label htmlFor="category" className="form-label">Category</label>
                                     <input type="text" className="form-control" id="category" name="category" value={transaction.category} onChange={handleChange} required />
@@ -81,8 +87,30 @@ function Ledger({ showAlert }) {
             </form>
 
             <div className=' d-flex flex-column align-items-center my-5'>
+                {transactions.length > 0 && (
+                    <div className='d-flex gap-3'>
+                        <div className='rounded border border-1 bg-success p-2 mb-3'>
+                            Income : {transactions.reduce((accu, transaction) => {
+                                if (transaction.type === 'income') {
+                                    return accu + transaction.amount;
+                                }
+                                return accu;
+                            }, 0) || 0}
+                        </div>
+                        <div className='rounded border border-1 bg-danger p-2 mb-3'>
+                            Expense : {transactions.reduce((accu, transaction) => {
+                                if (transaction.type === 'expense') {
+                                    return accu + transaction.amount;
+                                }
+                                return accu;
+                            }, 0) || 0}
+                        </div>
+                    </div>
+
+                )
+                }
                 {transactions.length > 0 ? transactions.map((transaction) =>
-                    <Transaction key={transaction._id} transaction={transaction} showAlert={showAlert} editTransaction={editTransaction}  />
+                    <Transaction key={transaction._id} transaction={transaction} showAlert={showAlert} editTransaction={editTransaction} />
                 ) :
                     (<div>No transactions</div>)
                 }
